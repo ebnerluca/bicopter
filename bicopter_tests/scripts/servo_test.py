624cf6aa2c9a9455ce1775d1ab56a5254c2fb92a
Luca Ebner <ebnerl@ethz.ch>
2021-10-27 19:48:56 +0200

    Modified servo_test.py to test updated pulse widths

--- bicopter_tests/scripts/servo_test.py
+++ bicopter_tests/scripts/servo_test.py
@@ -1,16 +1,18 @@
-import RPi.GPIO as GPIO
-from  gpiozero import Servo
+from gpiozero.pins.pigpio import PiGPIOFactory
+from  gpiozero import Servo, Device, AngularServo
 import time
 
+Device.pin_factory = PiGPIOFactory()
+
 servo_gpio_pin = 17
-servo = Servo(servo_gpio_pin)
+servo = AngularServo(servo_gpio_pin, min_pulse_width=0.5/1000.0, max_pulse_width=2.5/1000.0)
 
 while True:
-	servo.value = -1
+	servo.min()
 	time.sleep(2)
-	servo.value = 0
+	servo.max()
 	time.sleep(2)
-	servo.value = 1
+	servo.mid()
 	time.sleep(2)
 
 
