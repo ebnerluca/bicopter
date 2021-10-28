8d719bf169a0a357dbd09062ad3434d0de106692
Luca Ebner <ebnerl@ethz.ch>
2021-10-28 19:20:13 +0200

    Updated motor test script

--- bicopter_tests/scripts/motor_test.py
+++ bicopter_tests/scripts/motor_test.py
@@ -1,26 +1,54 @@
-from gpiozero import Servo
+from gpiozero import Servo, Device
+from gpiozero.pins.pigpio import PiGPIOFactory
 import time
 
-motor_left_gpio_pin = 5
-motor_left_min_signal = 1. / 1000.
-motor_left_max_signal = 1.5 / 1000.  # max 50% speed
-motor_arm_signal = 1.25 / 1000.
+motor_left_gpio_pin = 17
+motor_left_min_signal = 0.001
+motor_left_max_signal = 0.0015  # max 50% speed
+motor_arm_signal = 0.0011
+initial_val = -1
+
+
 motor_arm_value = ((motor_arm_signal - motor_left_min_signal) * 2.0) / (motor_left_max_signal - motor_left_min_signal) - 1.0  # find servo value between [-1,1] that corresponds to arm signal
 print("motor arm value: " + str(motor_arm_value))
 time.sleep(2)
-print("Initializing servo with initial value: None ...")
-time.sleep(2)
+
+print("Initializing servo with initial value: " + str(initial_val))
+
+Device.pin_factory = PiGPIOFactory()  # reduced servo jitter
+
 motor_left = Servo(pin=motor_left_gpio_pin,
-                   initial_value=-1.0,
+                   initial_value=None,
                    min_pulse_width=motor_left_min_signal,
                    max_pulse_width=motor_left_max_signal)
-print("Done.")
 
-print("Waiting 3 sec before arming...")
-time.sleep(3)
+print("Current pulse width: " + str(motor_left.pulse_width))
+
+key=True
+while(key):
+    try:
+        print(str(motor_left.pulse_width))
+    except KeyboardInterrupt:
+        key=False
+
+motor_left.value = -1
+print("Current pulse width: " + str(motor_left.pulse_width))
+key=True
+while(key):
+    try:
+        print(str(motor_left.pulse_width))
+    except KeyboardInterrupt:
+        key=False
+
 motor_left.value = motor_arm_value
-print("ARMED.")
+print("Current pulse width: " + str(motor_left.pulse_width))
+key=True
+while(key):
+    try:
+        print(str(motor_left.pulse_width))
+    except KeyboardInterrupt:
+        key=False
 
-time.sleep(2)
 
-
+print("Cleanup ...")
+motor_left.value=None
