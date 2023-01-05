6175ab44e937b56f9c1c5c49c114c004768d333f
Luca Ebner <ebnerl@ethz.ch>
2023-01-05 21:18:27 +1100

    Ported bicopter_model to ros2, fixed urdf test script

--- bicopter_model/urdf/urdf_test.py
+++ bicopter_model/urdf/urdf_test.py
@@ -5,35 +5,43 @@ import time
 
 physics_client_id = p.connect(p.GUI)
 
-p.setGravity(0, 0, -10)
-p.setRealTimeSimulation(True)
+p.setGravity(0, 0, -9.81)
+p.setRealTimeSimulation(False)
 
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
 planeId = p.loadURDF("plane.urdf", basePosition=[0.0, 0.0, 0.0])
-bicopterId = p.loadURDF("/home/ebnerl/catkin_ws/src/bicopter/bicopter_pybullet_controller/urdf/bicopter_urdf.urdf")
+bicopterId = p.loadURDF("/home/ebnerl/dev_ws/src/bicopter/bicopter_model/urdf/bicopter_urdf.urdf")
 
-index = 0.0
-index_step = 0.00001
-rate = 200  # Hz
+# sim frequency
+rate = 240.0  # Hz
 
+# pybullet pos controller gains
 kp = [1, 1]
 kd = [0.1, 0.1]
 
+# rotor speeds
+speed_left = 1.0
+speed_right = 1.0
+
+t = 0.0
 while True:
-    servo_left = 1.2 * math.sin(index)
-    servo_right = 1.2 * -math.sin(index)
-    index = index + index_step
+
+    # step
+    t = t + 1.0/rate
+    p.stepSimulation()
+
+    # set servo positions
+    servo_left = 0.78 * math.sin(t)
+    servo_right = 0.78 * -math.sin(t)
     p.setJointMotorControlArray(bodyUniqueId=bicopterId, jointIndices=[0, 1], controlMode=p.POSITION_CONTROL,
                                 targetPositions=[servo_left, servo_right],
                                 targetVelocities=[0.0, 0.0],
                                 positionGains=kp,
                                 velocityGains=kd)
 
-    speed_left = 10.0
-    speed_right = 10.0
     p.applyExternalForce(objectUniqueId=bicopterId, linkIndex=0, forceObj=[0.0, 0.0, speed_left], posObj=[0.0, 0.0, 0.0], flags=p.LINK_FRAME)
     p.applyExternalForce(objectUniqueId=bicopterId, linkIndex=1, forceObj=[0.0, 0.0, speed_right], posObj=[0.0, 0.0, 0.0], flags=p.LINK_FRAME)
 
-    time.sleep(1/rate)
+    time.sleep(1.0/rate)
 
