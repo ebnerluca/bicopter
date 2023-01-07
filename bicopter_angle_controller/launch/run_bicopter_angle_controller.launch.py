c58a2504628d0c3ed181549d6202c1693dcab5e2
Luca Ebner <ebnerl@todo.com>
2023-01-07 11:58:09 +1100

    Launchfiles cleanup

--- bicopter_angle_controller/launch/run_bicopter_angle_controller.launch.py
+++ bicopter_angle_controller/launch/run_bicopter_angle_controller.launch.py
@@ -2,14 +2,10 @@ from launch import LaunchDescription
 from launch.substitutions import PathJoinSubstitution
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
-from ament_index_python.packages import get_package_share_directory
-import os
 
 def generate_launch_description():
 
     ld = LaunchDescription()
-
-    # controller_params = os.path.join(get_package_share_directory('bicopter_angle_controller'), 'config', 'default.yaml')
 
     controller_params = PathJoinSubstitution(
         [
@@ -18,13 +14,6 @@ def generate_launch_description():
             "default.yaml"
         ]
     )
-    # model_params = PathJoinSubstitution(
-    #     [
-    #         FindPackageShare("bicopter_model"),
-    #         "urdf",
-    #         "bicopter_properties.yaml"
-    #     ]
-    # )
 
     node = Node(package='bicopter_angle_controller',
                 executable='bicopter_angle_controller',
@@ -33,5 +22,7 @@ def generate_launch_description():
                 output="screen",
                 emulate_tty=True
                 )
+
     ld.add_action(node)
+
     return ld
